2d464a118f5ebeb2a52cc0f15eed94a982a6a310
Kevin Garzon <kevin.garzon@mail.escuelaing.edu.co>
2021-04-12 20:04:04 -0500

    Injectado need

--- src/main/java/edu/eci/cvds/services/impl/SolidaridadServicesImpl.java
+++ src/main/java/edu/eci/cvds/services/impl/SolidaridadServicesImpl.java
@@ -1,6 +1,12 @@
 package edu.eci.cvds.services.impl;
 
+import edu.eci.cvds.persistence.NeedDAO;
 import edu.eci.cvds.services.SolidaridadServices;
 
+import javax.inject.Inject;
+
 public class SolidaridadServicesImpl implements SolidaridadServices {
+
+    @Inject
+    private NeedDAO needDAO;
 }
