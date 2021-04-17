2c79dc2121897805174f83ac48a8e416a62c9b70
Nicolas <onpc1224@gmail.com>
2021-04-17 09:57:38 -0500

    consulta categorias, categories.xhtml

--- src/main/java/edu/eci/cvds/persistence/mybatisimpl/mappers/CategoryMapper.java
+++ src/main/java/edu/eci/cvds/persistence/mybatisimpl/mappers/CategoryMapper.java
@@ -3,7 +3,7 @@ package edu.eci.cvds.persistence.mybatisimpl.mappers;
 import edu.eci.cvds.entities.Category;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.Date;
+import java.util.List;
 
 public interface CategoryMapper {
 
@@ -12,4 +12,6 @@ public interface CategoryMapper {
     public void modifyCategory(@Param("category") Category category);
 
     public Category loadC(@Param("id") int categoryId);
+
+    public List<Category> loadAllC();
 }
--- src/main/java/edu/eci/cvds/services/SolidaridadServices.java
+++ src/main/java/edu/eci/cvds/services/SolidaridadServices.java
@@ -3,6 +3,8 @@ package edu.eci.cvds.services;
 import edu.eci.cvds.entities.Category;
 import edu.eci.cvds.entities.Need;
 
+import java.util.List;
+
 public interface SolidaridadServices {
 
     public void registerCategory(Category c) throws ServicesException;
@@ -12,4 +14,6 @@ public interface SolidaridadServices {
     public void registerNeed(Need need) throws ServicesException;
 
     public Category loadCategory(int categoryId) throws ServicesException;
+
+    public List<Category> loadCategories() throws ServicesException;
 }
