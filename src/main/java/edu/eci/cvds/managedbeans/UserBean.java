d6fee943be79e500c123be1b28572564d0dd7f2f
Daniela Pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-04-19 22:02:17 -0500

    Apache Shiro

--- src/main/java/edu/eci/cvds/managedbeans/UserBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/UserBean.java
@@ -10,29 +10,32 @@ import org.apache.shiro.util.Factory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Level;
+
 @SessionScoped
-@ManagedBean(name="userBean")
+@ManagedBean(name="userBean",eager = true)
 public class UserBean implements Serializable {
     private static final Logger logger = LoggerFactory.getLogger(UserBean.class);
     private String username;
     private String userpassword;
-    private String redirectURL="/faces/index.html";
+    private String redirectURL="/faces/menu.xhtml";
     Subject subject;
 
     public void signin(){
-        Factory<SecurityManager> factory=new IniSecurityManagerFactory("classpath:shiro.ini");
-        SecurityManager securityManager=factory.getInstance();
-        SecurityUtils.setSecurityManager(securityManager);
+        //Factory<SecurityManager> factory=new IniSecurityManagerFactory("classpath:shiro.ini");
+        //SecurityManager securityManager=factory.getInstance();
+        //SecurityUtils.setSecurityManager(securityManager);
         subject= SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(getUsername(),new Sha256Hash(getUserpassword()).toHex());
         try {
             subject.login(token);
-            FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/registerCategory.xhtml");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/menu.xhtml");
 
         } catch ( UnknownAccountException e ) {
             //username wasn't in the system, show them an error message?
@@ -50,6 +53,19 @@ public class UserBean implements Serializable {
             e.printStackTrace();
         }
     }
+    public void logOut() {
+
+        SecurityUtils.getSubject().logout();
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(redirectURL);
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    private void facesError(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+    }
+
 
 
     public String getUsername() {
