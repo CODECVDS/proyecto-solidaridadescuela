1274d8b0af0a633917a1db52a180809bc1b1a11a
Cristhian <cristhian.torres@mail.escuelaing.edu.co>
2021-04-25 13:27:16 -0500

    SQL

--- src/main/java/edu/eci/cvds/persistence/NeedDAO.java
+++ src/main/java/edu/eci/cvds/persistence/NeedDAO.java
@@ -1,13 +1,14 @@
 package edu.eci.cvds.persistence;
 
 import edu.eci.cvds.entities.Need;
+import edu.eci.cvds.entities.Status;
 
 import java.util.List;
 
 public interface NeedDAO {
     public void save(Need necesidad) throws PersistenceException;
 
-    public void updateNeed(Need necesidad) throws PersistenceException;
+    public void updateNeedStatus(Status status) throws PersistenceException;
 
     public List<Need> needs() throws PersistenceException;
 
